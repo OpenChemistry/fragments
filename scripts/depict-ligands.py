5a43557dbcd79b48603919b85e34505b1894518c
Norwid Behrnd <nbehrnd@yahoo.com>
2023-12-06 14:09:39 +0100

    copy RDKit example about dative bonds
    The RDKit cookbook includes an example how to identify and mark
    dative bonds.[1]  The snippet of code is copy-pasted verbatim as
    provided by the page; credit is due to Greg Landrum.
    
    [1] http://rdkit.org/docs/Cookbook.html#organometallics-with-dative-bonds
    
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- scripts/depict-ligands.py
+++ scripts/depict-ligands.py
@@ -34,6 +34,35 @@ def svgDepict(mol):
     svg = d2d.GetDrawingText()
     return svg
 
+
+def is_transition_metal(at):
+    n = at.GetAtomicNum()
+    return (n>=22 and n<=29) or (n>=40 and n<=47) or (n>=72 and n<=79)
+
+
+def set_dative_bonds(mol, fromAtoms=(7,8)):
+    """ convert some bonds to dative
+
+    Replaces some single bonds between metals and atoms with atomic numbers in fomAtoms
+    with dative bonds. The replacement is only done if the atom has "too many" bonds.
+
+    Returns the modified molecule.
+
+    """
+    pt = Chem.GetPeriodicTable()
+    rwmol = Chem.RWMol(mol)
+    rwmol.UpdatePropertyCache(strict=False)
+    metals = [at for at in rwmol.GetAtoms() if is_transition_metal(at)]
+    for metal in metals:
+        for nbr in metal.GetNeighbors():
+            if nbr.GetAtomicNum() in fromAtoms and \
+               nbr.GetExplicitValence()>pt.GetDefaultValence(nbr.GetAtomicNum()) and \
+               rwmol.GetBondBetweenAtoms(nbr.GetIdx(),metal.GetIdx()).GetBondType() == Chem.BondType.SINGLE:
+                rwmol.RemoveBond(nbr.GetIdx(),metal.GetIdx())
+                rwmol.AddBond(nbr.GetIdx(),metal.GetIdx(),Chem.BondType.DATIVE)
+    return rwmol
+
+
 # repeat through all the files on the command-line
 # we can change this to use the glob module as well
 #  e.g., find all the files in a set of folders
